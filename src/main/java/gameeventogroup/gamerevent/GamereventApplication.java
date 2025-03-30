582bc3cfe2d2a250d4a78f23be8cbdb014542382
liamoctavio <ocmolina47@gmail.com>
2025-03-29 21:41:21 -0300

    nuevo inicio

---
+++ src/main/java/gameeventogroup/gamerevent/GamereventApplication.java
@@ -0,0 +1,13 @@
+package gameeventogroup.gamerevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamereventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamereventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gameeventogroup/gamerevent/GamereventApplicationTests.java
@@ -0,0 +1,13 @@
+package gameeventogroup.gamerevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamereventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
