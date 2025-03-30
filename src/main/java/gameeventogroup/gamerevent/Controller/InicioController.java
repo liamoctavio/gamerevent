523a4dca2cd74c759921c0296386480460fa3b0e
liamoctavio <ocmolina47@gmail.com>
2025-03-30 15:23:02 -0300

    inicio

---
+++ src/main/java/gameeventogroup/gamerevent/Controller/InicioController.java
@@ -0,0 +1,17 @@
+package gameeventogroup.gamerevent.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InicioController {
+
+    @GetMapping("/inicio")
+    public String mostrarInicio(Model model) {
+        // Puedes agregar datos al modelo si es necesario
+        // model.addAttribute("mensaje", "Bienvenido!");
+        return "inicio"; // Nombre del archivo HTML (sin extensión)
+    }
+}
